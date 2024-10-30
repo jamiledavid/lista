0b9dd9d2de07a3ade802169b2915827b64493084
jamiledavid <142550303+jamiledavid@users.noreply.github.com>
2024-10-29 21:15:48 -0400

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { SafeAreaView } from 'react-native';
+
+import Home from './../screens/Home';
+
+
+
+export default function App() {
+
+  return (
+
+    <SafeAreaView style={{ flex: 2 }}>
+
+      <Home />
+
+    </SafeAreaView>
+
+  );
+
+}
